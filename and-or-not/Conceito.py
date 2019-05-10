249e9af4d892f129f8c60dee44d8bc55553b390d
Elioenas Batista de Sousa <30697363+elioenas@users.noreply.github.com>
2019-05-09 22:54:07 -0300

    Add files via upload

---
+++ and-or-not/Conceito.py
@@ -0,0 +1,15 @@
+#and
+
+#Se ambos os operandos forem verdadeiros, a condição se tornará verdadeira.
+
+#or
+
+#Se algum dos dois operandos for diferente de zero, a condição se tornará verdadeira.
+
+#not
+
+#Usado para inverter o estado lógico de seu operando.
+
+#is
+
+#Avalia para verdadeiro se as variáveis ​​em ambos os lados do operador apontam para o mesmo objeto e false em caso contrário.
